8ffee2aa37c9d8ef310e3273af3876953584cd77
Chris Phillips <chris.phillips@mindmatrix.net>
2023-11-29 15:27:52 -0500

    example ironpdf

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # IronPdf
+
+1. Build docker image with custom fonts, note that we also tried running the ironpdf container directly and using `docker cp` to copy the files as well
+```shell
+docker build docker -t ironpdf
+```
+1. Run IronPdf in docker
+```shell
+cd src
+docker run -d --name=ironpdf -p 33350:33350 ironpdf
+```
---
+++ src/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Net;
+using IronPdf;
+
+// ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls13;
+
+//IronPdf.License.LicenseKey = "";
+IronPdf.Installation.ConnectToIronPdfHost(new IronPdf.GrpcLayer.IronPdfConnectionConfiguration()
+{
+    ConnectionType = IronPdf.GrpcLayer.IronPdfConnectionType.Docker,
+    Host = "localhost",
+    Port = 33350,
+});
+IronPdf.Installation.SkipShutdown = true;
+IronPdf.Installation.SkipInitialization = true;
+IronPdf.Installation.AutomaticallyDownloadNativeBinaries = false;
+
+ChromePdfRenderer renderer = new ChromePdfRenderer();
+PdfDocument pdf = renderer.RenderHtmlAsPdf("<span style=\"font-family: 'Caecilia LT Std'\">THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG<span>");
+pdf.SaveAs("ironpdf.pdf");
